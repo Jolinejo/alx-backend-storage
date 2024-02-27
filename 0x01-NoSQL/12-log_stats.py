41347ca69ce44ed413503fc55b2aa45c0a064d01
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-27 12:22:48 -0800

    task

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" some logs """
+
+import pymongo
+
+
+def log():
+    """logging"""
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    collection = db['nginx']
+
+    logs = collection.count_documents({})
+
+    print("{} logs".format(logs))
+    print("Methods: ")
+    listi = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for elem in listi:
+        num = collection.count_documents({"method": elem})
+        print("\tmethod {}: {}".format(elem, num))
+
+    stat = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(stat))
+
+if __name__ == "__main__":
+    log()
