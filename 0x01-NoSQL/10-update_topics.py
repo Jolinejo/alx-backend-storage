b91d454e672a671b7947503c6ec68642b1014d67
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-27 11:52:08 -0800

    task

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     """ update topics based on name """
     query = {"name": name}
     newval = {"$set": {"topics": topics}}
-    db.mongo_collection.update_many(query, newval)
+    mongo_collection.update_many(query, newval)
