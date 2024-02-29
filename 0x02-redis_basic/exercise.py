cd9f08d24104c2413f7c4acc818b8b0d2d97007e
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-29 00:04:44 +0000

    task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ redis ex
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -20,3 +20,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None):
+        """convert into original type"""
+        val = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return val
+
+    def get_str(self, key: str):
+        """convert to str"""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str):
+        """convert to int"""
+        return self.get(key, int)
